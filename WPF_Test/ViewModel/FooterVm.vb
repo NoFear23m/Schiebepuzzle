f96413543fdf6bd258ee7fd8f8e45257166cf953
NoFear23m <patschka.sascha@live.com>
2019-03-17 19:25:04 +0100

    Update für Fußnote

--- WPF_Test/ViewModel/FooterVm.vb
+++ WPF_Test/ViewModel/FooterVm.vb
@@ -2,7 +2,7 @@
     Public Class FooterVm
         Inherits ViewModelBase
 
-        Public Property Footertext As String = "Powered by Sascha Patschka"
+        Public Property Footertext As String = "Powered by Sascha Patschka - Idea by flori2212"
 
     End Class
 End Namespace
